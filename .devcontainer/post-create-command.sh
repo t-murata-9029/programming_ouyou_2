cad1652376de67196ec91a45a07e832235b87441
Toshihide Murata(村田 利秀) <114552280+t-murata-9029@users.noreply.github.com>
2025-10-21 07:10:38 +0000

    flask

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -5,6 +5,15 @@ echo "--- Running post-create-command.sh ---"
 
 # --- 実行したいコマンドを追記していきます ---
 
+# --- このスクリプトは、コンテナ作成後に一度だけ実行されます ---
+echo "--- Running post-create-command.sh ---"
+
+# --- 実行したいコマンドを追記していきます ---
+
+# 以下の 3行を追加、必要なのは pip のところ
+# Python (Flask) の依存関係をインストール
+echo "--- Installing Python dependencies... ---"
+pip install --user -r flask-app/requirements.txt
 
 # --- スクリプト終了 ---
 echo "--- Setup completed successfully! ---"
